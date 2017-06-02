28e4529aa9423d25608be4b2cf29d3c04d2e1ae2
Marco Maggi <marco.maggi-ipsu@poste.it>
2017-06-02 08:22:37 +0200

    development of conditions

--- src/ccexceptions.h
+++ src/ccexceptions.h
@@ -478,13 +478,11 @@ cce_condition_is_h_errno (const cce_condition_t * C)
 
 #define cce_condition_h_errno(S)					\
   _Generic((S),								\
-	   cce_condition_h_errno_t		*: (S),			\
-	   cce_location_t			*: (cce_condition_h_errno_t *)CCE_CLOC(S), \
-	   cce_condition_t			*: (cce_condition_h_errno_t *)(S), \
-	   cce_condition_root_t			*: (cce_condition_h_errno_t *)(S), \
-	   const cce_condition_t		*: (cce_condition_h_errno_t *)(S), \
-	   const cce_condition_root_t		*: (cce_condition_h_errno_t *)(S), \
-	   const cce_condition_h_errno_t	*: (cce_condition_h_errno_t *)(S))
+	   cce_location_t			*: (const cce_condition_h_errno_t *)CCE_CLOC(S), \
+	   cce_condition_t			*: (const cce_condition_h_errno_t *)(S), \
+	   cce_condition_h_errno_t		*: (const cce_condition_h_errno_t *)(S), \
+	   const cce_condition_t		*: (const cce_condition_h_errno_t *)(S), \
+	   const cce_condition_h_errno_t	*: (const cce_condition_h_errno_t *)(S))
 
 
 /** --------------------------------------------------------------------
