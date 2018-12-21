4a1e742b8047415e5bf74ecaf01e2850354d566d
Marco Maggi <marco.maggi-ipsu@poste.it>
2018-12-21 11:03:39 +0100

    fixing test file

--- tests/exception-with-handlers.c
+++ tests/exception-with-handlers.c
@@ -158,7 +158,6 @@ static void
 test_dynamically_allocated_clean_handler (cce_condition_t const * C CCE_UNUSED, cce_handler_t * _H)
 {
   CCE_PC(test_dynamically_allocated_clean_handler_t, H, _H);
-  free(H->handler.handler.pointer);
   *(H->flagp) = true;
   free(H);
   if (1) { fprintf(stderr, "%s: done\n", __func__); }
@@ -168,7 +167,6 @@ static void
 test_dynamically_allocated_error_handler (cce_condition_t const * C CCE_UNUSED, cce_handler_t * _H)
 {
   CCE_PC(test_dynamically_allocated_error_handler_t, H, _H);
-  free(H->handler.handler.pointer);
   *(H->flagp) = true;
   free(H);
   if (1) { fprintf(stderr, "%s: done\n", __func__); }
@@ -206,17 +204,17 @@ test_dynamically_allocated_handlers (void)
   cce_location_t	L[1];
   test_dynamically_allocated_clean_handler_t * volatile H1;
   test_dynamically_allocated_error_handler_t * volatile H2;
-  volatile bool		flag1 = false;
-  volatile bool		flag2 = false;
+  bool volatile		flag1 = false;
+  bool volatile		flag2 = false;
 
   if (cce_location(L)) {
     cce_run_catch_handlers_final(L);
   } else {
     H1 = test_dynamically_allocated_new_clean_handler(L, &flag1);
-    cce_register_clean_handler(L, (cce_clean_handler_t *)&(H1->handler));
+    cce_register_clean_handler(L, &(H1->handler));
 
     H2 = test_dynamically_allocated_new_error_handler(L, &flag2);
-    cce_register_error_handler(L, (cce_error_handler_t *)&(H2->handler));
+    cce_register_error_handler(L, &(H2->handler));
 
     if (1) {
       cce_raise(L, NULL);
