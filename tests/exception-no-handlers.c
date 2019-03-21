6e16c930b73f5b01473925a4d1bfc7bc448df326
Marco Maggi <marco.maggi-ipsu@poste.it>
2019-03-21 06:02:27 +0100

    tweaking tests program

--- tests/exception-no-handlers.c
+++ tests/exception-no-handlers.c
@@ -7,23 +7,19 @@
 
 
 
-  Copyright (C) 2016, 2017, 2018 Marco Maggi <marco.maggi-ipsu@poste.it>
+  Copyright (C) 2016, 2017, 2018, 2019 Marco Maggi <marco.maggi-ipsu@poste.it>
 
-  This is free software; you can  redistribute it and/or modify it under
-  the terms of the GNU Lesser General Public License as published by the
-  Free Software  Foundation; either version  3.0 of the License,  or (at
-  your option) any later version.
+  This is free software; you can redistribute  it and/or modify it under the terms of
+  the GNU Lesser General Public License as published by the Free Software Foundation;
+  either version 3.0 of the License, or (at your option) any later version.
 
-  This library  is distributed in the  hope that it will  be useful, but
-  WITHOUT   ANY  WARRANTY;   without  even   the  implied   warranty  of
-  MERCHANTABILITY  or FITNESS  FOR A  PARTICULAR PURPOSE.   See  the GNU
-  Lesser General Public License for more details.
+  This library  is distributed in the  hope that it  will be useful, but  WITHOUT ANY
+  WARRANTY; without  even the implied  warranty of  MERCHANTABILITY or FITNESS  FOR A
+  PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
 
-  You  should have  received a  copy of  the GNU  Lesser  General Public
-  License along  with this library; if  not, write to  the Free Software
-  Foundation, Inc.,  59 Temple Place,  Suite 330, Boston,  MA 02111-1307
-  USA.
-
+  You should have received a copy of the GNU Lesser General Public License along with
+  this library; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+  Suite 330, Boston, MA 02111-1307 USA.
 */
 
 
@@ -42,7 +38,7 @@ main (int argc CCE_UNUSED, char const *const CCE_UNUSED argv[] CCE_UNUSED)
 
     if (cce_location(L)) { // the error handler
       flag = false;
-      cce_run_catch_handlers(L);
+      cce_run_catch_handlers_final(L);
     } else { // the body
       cce_raise(L, NULL);
       flag = true;
@@ -59,7 +55,7 @@ main (int argc CCE_UNUSED, char const *const CCE_UNUSED argv[] CCE_UNUSED)
     switch (cce_location(L)) {
     case CCE_ERROR:
       flag = false;
-      cce_run_catch_handlers(L);
+      cce_run_catch_handlers_final(L);
       break;
 
     default:
