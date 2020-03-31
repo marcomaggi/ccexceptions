76495d26dab9bd5c85f0b99624b70b5c165f8bc2
Marco Maggi <marco.maggi-ipsu@poste.it>
2020-03-31 16:18:27 +0200

    development in usage of cclibraries.h

--- examples/plain-header.h
+++ examples/plain-header.h
@@ -14,7 +14,7 @@
 	definition is a "plain" one:  no inline functions; new condition
 	objects allocated by a constructor.
 
-  Copyright (C) 2017, 2019 Marco Maggi <mrc.mgg@gmail.com>
+  Copyright (C) 2017, 2019, 2020 Marco Maggi <mrc.mgg@gmail.com>
 
   The author  hereby grant permission to  use, copy, modify, distribute,  and license
   this  software  and its  documentation  for  any  purpose, provided  that  existing
@@ -74,18 +74,20 @@ struct my_condition_error_1_t {
 };
 
 extern void cce_descriptor_set_parent_to(my_descriptor_error_1_t) (cce_descriptor_t * D)
-  __attribute__((__nonnull__(1)));
+  CCLIB_FUNC_ATTRIBUTE_NONNULL(1);
 
 /* ------------------------------------------------------------------ */
 
 extern void my_condition_init_error_1 (cce_destination_t L, my_condition_error_1_t * C, int the_data)
-  __attribute__((__nonnull__(1)));
+  CCLIB_FUNC_ATTRIBUTE_NONNULL(1);
 
 extern cce_condition_t const * my_condition_new_error_1 (cce_destination_t L, int the_data)
-  __attribute__((__nonnull__(1),__returns_nonnull__));
+  CCLIB_FUNC_ATTRIBUTE_NONNULL(1)
+  CCLIB_FUNC_ATTRIBUTE_RETURNS_NONNULL;
 
 extern bool my_condition_is_error_1 (cce_condition_t const * C)
-  __attribute__((__pure__,__nonnull__(1)));
+  CCLIB_FUNC_ATTRIBUTE_PURE
+  CCLIB_FUNC_ATTRIBUTE_NONNULL(1);
 
 
 /** --------------------------------------------------------------------
